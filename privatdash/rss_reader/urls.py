06dcd1dc1f831456130454ca5a164f33e87a3da3
Max Brauer <max.brauer@inqbus.de>
2014-01-19 11:09:21 +0100

    added listview for rss source

--- privatdash/rss_reader/urls.py
+++ privatdash/rss_reader/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import RSSReaderView
+from .views import RSSReaderView, RSSSourceListView
 
 
 urlpatterns = patterns('',
     url(r'^$', RSSReaderView.as_view(), name='rss_reader_view'),
+    url(r'^list/$', RSSSourceListView.as_view(), name='rss_reader_rsssource_list_view'),
 )
--- privatdash/rss_reader/views.py
+++ privatdash/rss_reader/views.py
@@ -1,8 +1,21 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 from privatdash.views import ActiveNavMixin
 
+from .models import RSSSource
 
-class RSSReaderView(ActiveNavMixin, TemplateView):
+
+class RSSReaderBaseView(ActiveNavMixin):
     sidenav_active = 'rss_reader'
+
+
+class RSSReaderView(RSSReaderBaseView, TemplateView):
     template_name = 'rss_reader/rss_reader_view.html'
+
+
+class RSSSourceListView(RSSReaderBaseView, ListView):
+    model = RSSSource
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(RSSSourceListView, self).get_queryset(*args, **kwargs)
+        return qs.filter(user=self.request.user)
