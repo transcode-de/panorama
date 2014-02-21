6731c892ff27f3bfb8367d205cd4c525cd661f73
Max Brauer <max@transcode.de>
2014-02-21 09:20:37 +0100

    added core to installed apps and renamed context processor

--- privatdash/privatdash/settings.py
+++ privatdash/privatdash/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'crispy_forms',
 
     # own apps
+    'core',
     'rss_reader'
 )
 
@@ -101,7 +102,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
-    "privatdash.context_processors.sidenav"
+    "core.context_processors.sidenav"
 )
 
 if TEMPLATE_DEBUG:
